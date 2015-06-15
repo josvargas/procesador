c49acf45dd84ee8fc472df634ed40f7ee49122cd
jdvargasa <josue.david.vargas@gmail.com>
2015-06-15 10:16:16 -0600

    DefinitionsBranches file 15.06.15 10am

---
+++ work/DefinitionsBranches.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+`ifndef DEFINTIONS_V
+`define DEFINTIONS_V
+	
+`default_nettype none	
+`define JMP   6'h01
+`define BAEQ  6'h02
+`define BANE  6'h03
+`define BACS  6'h04
+`define BACC  6'h05
+`define BAMI  6'h06
+`define BAPL  6'h07
+`define BBEQ  6'h08
+`define BBNE  6'h09
+`define BBCS  6'h0A
+`define BBCC  6'h0B
+`define BBMI  6'h0C
+`define BBPL  6'h0D
+
+
+`endif
