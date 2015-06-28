f88bcf13c2e2536a85904b04faf682269c1bbe9b
jdvargasa <josue.david.vargas@gmail.com>
2015-06-28 15:33:36 -0600

    RAM file 28.06.15 3pm josvargas

--- work/RAM.v
+++ work/RAM.v
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ps
 
-module RAM_DUAL_READ_PORT # ( parameter DATA_WIDTH= 8, parameter ADDR_WIDTH=10, parameter MEM_SIZE=10 )
+module RAM_DUAL_READ_PORT # ( parameter DATA_WIDTH= 8, parameter ADDR_WIDTH=10, parameter MEM_SIZE=1023 )
 (
-	input wire						Clock,
-	input wire						iWriteEnable,
+	input wire			Clock,
+input wire				iWriteEnable,
 	input wire[ADDR_WIDTH-1:0]	iReadAddress,
 	input wire[ADDR_WIDTH-1:0]	iWriteAddress,
-	input wire[DATA_WIDTH-1:0]		 	iDataIn,
-	output reg [DATA_WIDTH-1:0] 		oDataOut
+	input wire[DATA_WIDTH-1:0]	iDataIn,
+	output reg [DATA_WIDTH-1:0] 	oDataOut
 );
 
 reg [DATA_WIDTH-1:0] Ram [MEM_SIZE-1:0];		
@@ -15,9 +15,9 @@ reg [DATA_WIDTH-1:0] Ram [MEM_SIZE-1:0];
 always @(posedge Clock) 
 begin 
 	
-		if (iWriteEnable) 
+		if (iWriteEnable) begin
 			Ram[iWriteAddress] <= iDataIn; 
-			
+		end	
 	
 		oDataOut <= Ram[iReadAddress]; 
 		
